6ab18c61c26a60a443d81b3eff7887c6ba7721d9
ellenhc <59638391+ellenhc@users.noreply.github.com>
2021-03-06 21:58:26 -0800

    prove 09 seems to work

--- index.js
+++ index.js
@@ -9,10 +9,11 @@ app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 app.get('/math', handleMath);
 app.get('/math_service', math_service);
+app.get('/getRate', handlePostage);
 app.get('/', (req, res) => res.render('pages/index'))
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
-
+//Team acitivty
 function handleMath(request, response) {
     let operand1 = Number(request.query.operand1);
     let operator = request.query.operator;
@@ -43,5 +44,86 @@ function calculate(operand1, operator, operand2) {
         answer = operand1 / operand2;
     }
     const params = { operator: operator, operand1: operand1, operand2: operand2, answer: answer };
+    return params;
+}
+
+//Prove activity
+function handlePostage(request, response) {
+    let mailType = Number(request.query.mailType);
+    let weight = Number(request.query.weight);
+    let params = calculateRate(mailType, weight);
+    response.render('pages/result', params);
+}
+
+function calculateRate(mailType, weight) {
+    let postage;
+    switch (mailType) {
+        case 1:
+            if (weight <= 1) {
+                postage = 0.55;
+            } else if (weight > 1 && weight <= 2) {
+                postage = 0.75;
+            } else if (weight > 2 && weight <= 3) {
+                postage = 0.95;
+            } else if (weight > 3 && weight <= 3.5) {
+                postage = 1.15;
+            }
+            break;
+        case 2:
+            if (weight <= 1) {
+                postage = 0.51;
+            } else if (weight > 1 && weight <= 2) {
+                postage = 0.71;
+            } else if (weight > 2 && weight <= 3) {
+                postage = 0.91;
+            } else if (weight > 3 && weight <= 3.5) {
+                postage = 1.11;
+            }
+            break;
+        case 3:
+            if (weight <= 1) {
+                postage = 1.00;
+            } else if (weight > 1 && weight <= 2) {
+                postage = 1.20;
+            } else if (weight > 2 && weight <= 3) {
+                postage = 1.40;
+            } else if (weight > 3 && weight <= 4) {
+                postage = 1.60;
+            } else if (weight > 4 && weight <= 5) {
+                postage = 1.80;
+            } else if (weight > 5 && weight <= 6) {
+                postage = 2.00;
+            } else if (weight > 6 && weight <= 7) {
+                postage = 2.20;
+            } else if (weight > 7 && weight <= 8) {
+                postage = 2.40;
+            } else if (weight > 8 && weight <= 9) {
+                postage = 2.60;
+            } else if (weight > 9 && weight <= 10) {
+                postage = 2.80;
+            } else if (weight > 10 && weight <= 11) {
+                postage = 3.00;
+            } else if (weight > 11 && weight <= 12) {
+                postage = 3.20;
+            } else if (weight > 12 && weight <= 13) {
+                postage = 3.40;
+            }
+            break;
+        case 4:
+            if (weight <= 4) {
+                postage = 4.00;
+            } else if (weight > 4 && weight <= 8) {
+                postage = 4.80;
+            } else if (weight > 8 && weight <= 12) {
+                postage = 5.50;
+            } else if (weight > 12 && weight <= 13) {
+                postage = 6.25;
+            }
+            break;
+        default:
+            postage = 0;
+            break;
+    }
+    const params = { mailType: mailType, weight: weight, postage: postage };
     return params;
 }
